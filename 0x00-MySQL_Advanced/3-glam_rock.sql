d87e55780cc5b836ecc491e231c4c90395900ecf
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-23 13:28:45 +0100

    glam rock3

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -2,5 +2,5 @@
 SELECT band_name, 
     (CASE WHEN `split` IS NULL THEN 2022 - formed ELSE `split` - formed END) AS lifespan 
 FROM metal_bands
-WHERE style = 'Glam rock'
+WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC
