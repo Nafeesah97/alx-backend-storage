e5d3ff4164eba5b92955bfcf2ba3e22fbad8fda7
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-23 23:37:47 +0100

    valid email

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -5,7 +5,7 @@ CREATE TRIGGER email_reset
 BEFORE UPDATE ON users
 FOR EACH ROW
 BEGIN
-    IF OLD.email != NEW.email
+    IF OLD.email != NEW.email THEN
         SET NEW.valid_email = 0;
     ELSE
         SET NEW.valid_email = NEW.valid_email;
