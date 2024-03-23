4d3f28420df2d280b3861593aa673e2e9ea73b76
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-23 13:21:43 +0100

    glam rock2

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
 -- lists all bands with Glam rock as their main style, ranked by their longevity
-SELECT band_name, (split - formed) AS lifespan 
+SELECT band_name, 
+    (CASE WHEN `split` IS NULL THEN 2022 - formed ELSE `split` - formed END) AS lifespan 
 FROM metal_bands
-WHERE style = 'Glam rock' AND formed <= 2022 AND split <= 2022
+WHERE style = 'Glam rock'
 ORDER BY lifespan DESC
