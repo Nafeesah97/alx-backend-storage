8633dec207e3cc756af5b6a8ef5641e24c983995
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-26 17:25:25 +0100

    logs2

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -7,12 +7,12 @@ client = MongoClient('mongodb://127.0.0.1:27017')
 log_collection= client.logs.nginx
 
 total_doc = log_collection.count_documents({})
-gets = log_collection.find({"method" : "GET"}).count_documents({})
-posts = log_collection.find({"method" : "POST"}).count_documents({})
-puts = log_collection.find({"method" : "PUT"}).count_documents({})
-patches = log_collection.find({"method" : "PATCH"}).count_documents({})
-deletes = log_collection.find({"method" : "DELETE"}).count_documents({})
-get_status = log_collection.find({ '$and': [{"method" : "GET"}, {"path" : "/status"}]}).count_documents({})
+gets = log_collection.count_documents({"method" : "GET"})
+posts = log_collection.count_documents({"method" : "POST"})
+puts = log_collection.count_documents({"method" : "PUT"})
+patches = log_collection.count_documents({"method" : "PATCH"})
+deletes = log_collection.count_documents({"method" : "DELETE"})
+get_status = log_collection.count_documents({ '$and': [{"method" : "GET"}, {"path" : "/status"}]})
 
 print('''
       {} logs
