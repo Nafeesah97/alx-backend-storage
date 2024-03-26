afa4cc7df601a26befdb414fca7636a6e6d0c8a0
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-26 23:30:24 +0100

    avg

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -6,13 +6,13 @@ from pymongo import DESCENDING
 def top_students(mongo_collection):
     """returns all students sorted by average score"""
     pipeline = [
-        {
-            "$group": {
-            "_id": None,
-            "averageScore": {"$avg": "topics"}
-            }
-        },
-        {"$sort": {"averageScore": -1}}
+        {"$unwind": "$scores"},
+        {"$group": {
+            "_id": "$_id",
+            "student_id": {"$first": "$_id"},
+            "average_score": {"$avg": "$scores.score"}
+        }},
+        {"$sort": {"average_score": -1}}
     ]
     result = list(mongo_collection.aggregate(pipeline))
     return result
