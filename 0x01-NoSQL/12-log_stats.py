9b2dcafde8eb30d14d5135fc2e079426b45db6e5
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-26 18:08:33 +0100

    fix 4

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -6,18 +6,21 @@ from pymongo import MongoClient
 def get_stats(log_collection):
     """get statistics about Nginx logs stored in MongoDB"""
     total_doc = log_collection.count_documents({})
+    print('{} logs'.format(total_doc))
+    print('Methods:')
     gets = log_collection.count_documents({"method": "GET"})
     posts = log_collection.count_documents({"method": "POST"})
     puts = log_collection.count_documents({"method": "PUT"})
     patches = log_collection.count_documents({"method": "PATCH"})
     deletes = log_collection.count_documents({"method": "DELETE"})
+    print('\tmethod GET: {}'.format(gets))
+    print('\tmethod POST: {}'.format(posts))
+    print('\tmethod PUT: {}'.format(puts))
+    print('\tmethod PATCH: {}'.format(patches))
+    print('\tmethod DELETE: {}'.format(deletes))
     get_status = log_collection.count_documents(
         {'$and': [{"method": "GET"}, {"path": "/status"}]})
-
-    print('''{} logs\nMethods:\n\tmethod GET: {}\n\tmethod POST: {}
-    \tmethod PUT:{}\n\tmethod PATCH: {}\n\tmethod DELETE: {}\
-            \n{} status check'''.format(
-            total_doc, gets, posts, puts, patches, deletes, get_status))
+    print('{} status check'.format(get_status))
 
 
 if __name__ == "__main__":
