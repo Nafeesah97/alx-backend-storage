faf219599d988ffd55550d18af6721991e8bae87
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-26 17:46:46 +0100

    logs5

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,19 +3,18 @@
 from pymongo import MongoClient
 
 
-client = MongoClient('mongodb://127.0.0.1:27017')
-log_collection = client.logs.nginx
-
-total_doc = log_collection.count_documents({})
-gets = log_collection.count_documents({"method": "GET"})
-posts = log_collection.count_documents({"method": "POST"})
-puts = log_collection.count_documents({"method": "PUT"})
-patches = log_collection.count_documents({"method": "PATCH"})
-deletes = log_collection.count_documents({"method": "DELETE"})
-get_status = log_collection.count_documents(
+def get_stats(log_collection):
+    """get statistics about Nginx logs stored in MongoDB"""
+    total_doc = log_collection.count_documents({})
+    gets = log_collection.count_documents({"method": "GET"})
+    posts = log_collection.count_documents({"method": "POST"})
+    puts = log_collection.count_documents({"method": "PUT"})
+    patches = log_collection.count_documents({"method": "PATCH"})
+    deletes = log_collection.count_documents({"method": "DELETE"})
+    get_status = log_collection.count_documents(
         {'$and': [{"method": "GET"}, {"path": "/status"}]})
 
-print('''{} logs
+    print('''{} logs
 Methods:
     method GET: {}
     method POST: {}
@@ -24,3 +23,9 @@ Methods:
     method DELETE: {}
 {} status check'''.format(
     total_doc, gets, posts, puts, patches, deletes, get_status))
+
+
+if __name__ == "__main__":
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    log_collection = client.logs.nginx
+    get_stats(log_collection)
