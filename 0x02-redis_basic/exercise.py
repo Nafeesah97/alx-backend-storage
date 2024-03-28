996fc9fa31195fcd075f2a4e5b92ad5fe090164f
Nafeesah97 <salaudeennafeesah97@gmail.com>
2024-03-28 00:38:41 +0000

    fixing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,7 +8,6 @@ from functools import wraps
 
 def count_calls(method: callable) -> callable:
     """count how many times methods of the Cache class are called"""
-    
     @wraps(method)
     def counter(self, *args, **kwargs):
         """increment the counter"""
@@ -35,6 +34,7 @@ def call_history(method: Callable) -> Callable:
         return output
     return retriver
 
+
 def replay(method: Callable) -> None:
     """Function to display the history of calls of a particular function."""
     cache_store = getattr(method.__self__, '_redis', None)
@@ -50,7 +50,7 @@ def replay(method: Callable) -> None:
         args_str = args.decode('utf-8')  # Convert bytes to string
         output_str = output.decode('utf-8')  # Convert bytes to string
         print(f"{method.__qualname__}(*{args_str}) -> {output_str}")
-        
+
 
 class Cache():
     """
@@ -60,7 +60,7 @@ class Cache():
         store(data): returns a string.
     """
 
-    def __init__ (self):
+    def __init__(self):
         """to instantiate"""
         self._redis = redis.Redis()
         self._redis.flushdb()
@@ -72,7 +72,7 @@ class Cache():
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
-    
+
     def get(self, key: str, fn: Optional[Callable] = None):
         """creat get method"""
         data = self._redis.get(key)
@@ -81,12 +81,11 @@ class Cache():
         if fn:
             return fn(data)
         return data
-    
+
     def get_str(self, key: str) -> str:
         """automatically parametrize Cache.get to str"""
         return self.get(key, fn=lambda d: d.decode("utf-8"))
-    
+
     def get_int(self, key: str) -> int:
         """automatically parametrize Cache.get to int"""
         return self.get(key, fn=int)
-    
